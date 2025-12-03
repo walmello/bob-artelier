3d0b57ae416583e4abd5724230b7061b2d807f4c
Walter Melo <41174751+walmello@users.noreply.github.com>
2025-12-03 15:18:10 -0300

    agora removeu vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
 });
